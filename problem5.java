10df3d8946f26ee42b83c3c9f380c5ad390ff822
Lakshmi Sesha Sai Maddineni <159765297+SeshaSai07@users.noreply.github.com>
2025-09-02 21:07:08 +0530

    Create problem5.java

---
+++ problem5.java
@@ -0,0 +1,27 @@
+Question:
+
+While loop is another loop like for loop but unlike for loop it only checks for one condition.
+Here, we will use a while loop and print a number n's table in reverse order.
+
+Examples:
+
+Input: n = 1
+Output: 10 9 8 7 6 5 4 3 2 1
+Input: n = 2
+Output: 20 18 16 14 12 10 8 6 4 2
+Constraints:
+1 <= n<= 1000
+
+Solution:
+
+import java.util.*;
+class Solution {
+    public void calculateMultiples(int n) {
+        // code here
+        int i = 10;
+        while(i >= 1) {
+            System.out.print(i * n  + " ");
+            i--;
+        }
+    }
+}
